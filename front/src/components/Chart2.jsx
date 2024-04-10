b0c162a83ccd5acb2ac0f98b2c086b35ac842e21
Szymon Ożóg <szymon.ozog@onet.eu>
2024-04-10 17:33:56 +0200

    Chart2

---
+++ front/src/components/Chart2.jsx
@@ -0,0 +1,59 @@
+'use client';
+import React, {useContext} from 'react';
+import {
+    ScatterChart,
+    Scatter,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+  } from "recharts";
+import { DataContext } from '../contexts/DataContext';
+import { ChartContext } from '../contexts/ChartContext';
+
+export const ChartComponent2 = () => {
+    const { data, variable, showCharts, types } = useContext(DataContext);
+
+    const { minIndex, maxIndex } = useContext(ChartContext);
+
+
+    if (showCharts) {
+        const dataType = types[variable];
+
+        let chartComponent2 = null;
+            if (!(dataType === 'Int64' || dataType === 'Float64')) {
+            chartComponent2 = (
+                <ResponsiveContainer width="100%" aspect={3}>
+                <ScatterChart
+                width={500}
+                height={300}
+                data={data.slice(minIndex, maxIndex)}
+                margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                }}
+                >
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey={data.timestamp} />
+                <YAxis dataKey={variable} type="number" name={variable} />
+                <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+                <Legend />
+                <Scatter name={variable} dataKey={variable} fill="#8884d8" />
+                </ScatterChart>
+                </ResponsiveContainer>
+            );
+        }
+
+        return (
+            <div>
+                {chartComponent2}
+            </div>
+        );
+    } else {
+        return <div></div>;
+    }
+}
--- front/src/pages/MainPage.jsx
+++ front/src/pages/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { CSVParser } from '../components/CSVParser';
 import { Table } from '../components/Table';
 import { ChartComponent } from '../components/Chart';
+import { ChartComponent2 } from '../components/Chart2';
 import { ChartPersonalizer } from '../components/ChartPersonalizer';
 
 
@@ -11,5 +12,6 @@ export const MainPage = () => {
         <Table></Table>
         <ChartPersonalizer></ChartPersonalizer>
         <ChartComponent></ChartComponent>
+        <ChartComponent2></ChartComponent2>
     </div> 
 }
